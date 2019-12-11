4b7fd389d3c5be91d4cfddaf3c53d88cfdc3fd76
leonleon123 <lele.slav@gmail.com>
2019-12-11 15:19:23 +0100

    Refactored day 1.

--- 01/main.py
+++ 01/main.py
@@ -1,7 +1,5 @@
 def rec_fuel(mass):
-    tmp = int(mass/3) - 2
-    return (tmp + rec_fuel(tmp)) if tmp > 0 else 0
+    return (int(mass/3)-2 + rec_fuel(int(mass/3)-2)) if int(mass/3)-2 > 0 else 0
 with open("input1.txt") as file:
-    print(sum([int(int(x)/3)-2 for x in file]))
-with open("input2.txt") as file:
-    print(sum([rec_fuel(int(x)) for x in file]))
+    data = file.readlines()
+    print(sum([int(int(x)/3)-2 for x in data]), sum([rec_fuel(int(x)) for x in data]), sep="\n")
