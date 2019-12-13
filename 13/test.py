729c0736bbb96f0a7d1d4b8cdf421fd2a02b712c
leonleon123 <lele.slav@gmail.com>
2019-12-13 15:25:10 +0100

    Day 13.

--- 13/test.py
+++
@@ -1,4 +0,0 @@
-with open("test_input2.txt") as file:
-    data = file.readline().split(",")[2:]
-    key_values = [" ", "█", "░", "▉","O"]
-    print(*["".join([key_values[int(y)] for y in data[x:x+44]]) for x in range(0,len(data),44)], sep="\n")
