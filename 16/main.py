9e92d1cda7192ca388cb14329a6d237c4fa34852
leonleon123 <lele.slav@gmail.com>
2019-12-16 11:28:28 +0100

    Refactoring

--- 16/main.py
+++ 16/main.py
@@ -1,11 +1,9 @@
 from numpy import dot, absolute, cumsum
-def generate_matrix(n):
-    repeat = [0,1,0,-1]
-    return [[repeat[int((j+1)/(i+1))%len(repeat)] for j in range(n)] for i in range(n)]
 with open("input1.txt") as file:
     num1 = [int(x) for x in file.readline().replace("\n","")]
     num2 = num1 * 10000
-    matrix1 = generate_matrix(len(num1))
+    repeat = [0,1,0,-1]
+    matrix1 = [[repeat[int((j+1)/(i+1))%len(repeat)] for j in range(len(num1))] for i in range(len(num1))]
     for i in range(100):
         num1 = absolute(dot(matrix1,num1)) % 10
     print("".join(str(x) for x in num1[:8]))
